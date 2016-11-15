d8518195a1770bc781fa3785f45454534a34bfa9
Anand Sundaram <ansundaram@paypal.com>
2016-11-15 23:16:29 +0800

    Using gorilla mux for routing

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
     "log"
     "net/http"
     "path/filepath"
+    "fmt"
+	"github.com/gorilla/mux"
 )
 
 var uploadFolderName = "uploaded"
 var pathSeparator = string(filepath.Separator)
 
 func main() {
-    http.HandleFunc("/", upload)
-    err := http.ListenAndServe(":9090", nil) // setting listening port
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", upload)
+    router.HandleFunc("/type/{type}", objectType)
+    router.HandleFunc("/type/{type}/id/{id}", objectTypeId)
+    router.HandleFunc("/type/{type}/id/{id}/time/{time}", objectTypeIdTime)
+    log.Fatal(http.ListenAndServe(":9090", router))
 }
