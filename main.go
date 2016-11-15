97fc0809a47f9da85a2cbed01253fee4efc5a279
Anand Sundaram <ansundaram@paypal.com>
2016-11-15 23:21:06 +0800

    Display all objects of one type

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "path/filepath"
     "fmt"
 	"github.com/gorilla/mux"
+	"strings"
 )
 
 var uploadFolderName = "uploaded"
@@ -18,4 +19,20 @@ func main() {
     router.HandleFunc("/type/{type}/id/{id}", objectTypeId)
     router.HandleFunc("/type/{type}/id/{id}/time/{time}", objectTypeIdTime)
     log.Fatal(http.ListenAndServe(":9090", router))
+}
+
+func objectType(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintln(w, "Object Type is ", vars["type"])
+
+	records := parseCsv(w, r, "upload.csv")
+
+	for i := 0; i < len(records); i++ {
+		if strings.Compare(records[i][1], vars["type"]) == 0 {
+			for j := 0; j < len(records[i]); j++ {
+	            fmt.Fprintf(w, records[i][j] + " ")
+	        }
+	        fmt.Fprintln(w)
+		}
+	}
 }
