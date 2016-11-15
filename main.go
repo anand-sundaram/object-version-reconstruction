a9f4e57172343796fc62d32f31aae23a8a37005a
Anand Sundaram <ansundaram@paypal.com>
2016-11-15 23:48:32 +0800

    Display state of object by type, id and time

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
     "net/http"
     "path/filepath"
     "fmt"
-	"github.com/gorilla/mux"
-	"strings"
+    "github.com/gorilla/mux"
+    "strings"
+    "strconv"
+    "encoding/json"
+    "sort"
 )
 
 var uploadFolderName = "uploaded"
@@ -52,4 +55,66 @@ func objectTypeId(w http.ResponseWriter, r *http.Request) {
 	        fmt.Fprintln(w)
 		}
 	}
+}
+
+func objectTypeIdTime(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Object Type is %v and Object ID is %v and the Time is %v", vars["type"], vars["id"], vars["time"])
+	fmt.Fprintln(w)
+
+	records := parseCsv(w, r, "upload.csv")
+
+	time, err := strconv.Atoi(vars["time"])
+	if err != nil {
+        fmt.Println(err)
+    }
+
+	properties := ""
+	var mapTimeToChanges map[int](map[string]string)
+
+	mapTimeToChanges = make(map[int](map[string]string))
+
+	for i := 0; i < len(records); i++ {
+		recordTimestamp, err := strconv.Atoi(records[i][2])
+		if err != nil {
+	        fmt.Println(err)
+	    }
+		if strings.Compare(records[i][1], vars["type"]) == 0 && strings.Compare(records[i][0], vars["id"]) == 0 && time >= recordTimestamp {
+			properties = properties + records[i][3] + ", "
+			for j := 0; j < len(records[i]); j++ {
+	            fmt.Fprintf(w, records[i][j] + " ")
+	        }
+	        fmt.Fprintln(w)
+			var dat map[string]string
+            if err := json.Unmarshal([]byte(records[i][3]), &dat); err != nil {
+		        fmt.Println(err)
+		    }
+		    mapTimeToChanges[recordTimestamp] = dat
+		    fmt.Println(dat)
+		    for key, value := range dat {
+			    fmt.Printf("key[%s] value[%s]\n", key, value)
+			}
+	        fmt.Fprintln(w)
+		}
+	}
+
+	var keys []int
+    for k := range mapTimeToChanges {
+        keys = append(keys, k)
+    }
+    sort.Ints(keys)
+
+    var objectStateMap map[string]string
+    objectStateMap = make(map[string]string)
+
+    for i := 0; i < len(keys); i++ {
+		var m map[string]string
+		m = make(map[string]string)
+		m = mapTimeToChanges[keys[i]]
+		for key, value := range m {
+		    fmt.Printf("key[%s] value[%s]\n", key, value)
+		    objectStateMap[key] = value
+		}
+    }
+    fmt.Fprintln(w, objectStateMap)
 }
