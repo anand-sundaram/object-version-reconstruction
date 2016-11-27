277896e19259b56c7e59cdaf19cd4ae09ad8931d
Anand Sundaram <ansundaram@paypal.com>
2016-11-27 15:02:22 +0800

    Created a struct to represent an object property change

--- parse.go
+++ parse.go
@@ -5,7 +5,18 @@ import(
     "net/http"
     "os"
     "encoding/csv"
+    "time"
+    "strconv"
+    "encoding/json"
 )
+
+type objectPropertyState struct {
+    objectId string
+    objectType string
+    timestamp time.Time
+    property string
+    value string
+}
 
 func parseCsv(w http.ResponseWriter, r *http.Request, filename string) [][]string {
     fmt.Println("method:", r.Method)
@@ -22,10 +33,42 @@ func parseCsv(w http.ResponseWriter, r *http.Request, filename string) [][]strin
         return nil
     }
     for i := 0; i < len(records); i++ {
+        createObjectPropertyState(records[i])
         for j := 0; j < len(records[i]); j++ {
             fmt.Print(records[i][j] + " ")
         }
         fmt.Println()
     }
     return records
+}
+
+func createObjectPropertyState(record []string) []objectPropertyState {
+    propertyValueMap := stringToPropertyValueMap(record[3])
+    opsArray := make([]objectPropertyState, 0)
+    t, err := strconv.ParseInt(record[2], 10, 64)
+    fmt.Println(time.Unix(t, 0))
+    if err != nil {
+        fmt.Println(err)
+        return nil
+    }
+    for k, v := range propertyValueMap {
+        ops := objectPropertyState{
+            objectId: record[0],
+            objectType: record[1],
+            timestamp: time.Unix(t, 0),
+            property: k,
+            value: v,
+        }
+        opsArray = append(opsArray, ops)
+    }
+    fmt.Println(opsArray)
+    return opsArray
+}
+
+func stringToPropertyValueMap(str string) map[string]string {
+    var dat map[string]string
+    if err := json.Unmarshal([]byte(str), &dat); err != nil {
+        fmt.Println(err)
+    }
+    return dat
 }
