6a70ce534b8b5f2a4110a6b4b16c34a9324699df
Anand Sundaram <ansundaram@paypal.com>
2016-11-28 22:15:58 +0800

    Querying the database to retrieve data instead of parsing the csv file

---
+++ get.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+    "net/http"
+    "fmt"
+    "github.com/gorilla/mux"
+    "encoding/json"
+    "time"
+    "database/sql"
+)
+
+func objectType(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	sqlStr := `SELECT * 
+		FROM object_property_state
+		WHERE object_type = ?`
+    fmt.Println("reached prepare")
+	rows, err := DBCon.Query(sqlStr, vars["type"])
+	if err != nil {
+		fmt.Println("error in querying")
+		panic(err)
+	}
+
+	printJsonOutput(rows, w)
+}
+
+func objectTypeId(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	sqlStr := `SELECT * 
+		FROM object_property_state
+		WHERE object_type = ?
+		AND object_id = ?`
+    fmt.Println("reached prepare")
+	rows, err := DBCon.Query(sqlStr, vars["type"], vars["id"])
+	if err != nil {
+		fmt.Println("error in querying")
+		panic(err)
+	}
+
+	printJsonOutput(rows, w)
+}
+
+func objectTypeIdTime(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	sqlStr := `SELECT ops.object_id, ops.object_type, ops.timestamp, ops.property, ops.value
+		FROM (
+			SELECT object_id, object_type, property, MAX(timestamp) AS timestamp
+			FROM object_property_state
+			WHERE object_type = ?
+			AND object_id = ?
+			AND UNIX_TIMESTAMP(timestamp) < ?
+			GROUP BY object_id, object_type, property
+			) AS ops_2,
+			object_property_state as ops
+		WHERE ops_2.object_id = ops.object_id
+		AND ops_2.object_type = ops.object_type
+		AND ops_2.property = ops.property
+		AND ops_2.timestamp = ops.timestamp`
+    fmt.Println("reached prepare")
+	rows, err := DBCon.Query(sqlStr, vars["type"], vars["id"], vars["time"])
+	if err != nil {
+		fmt.Println("error in querying")
+		panic(err)
+	}
+
+	printJsonOutput(rows, w)
+}
+
+func printJsonOutput(rows *sql.Rows, w http.ResponseWriter) {
+	objectPropertyStateSlice := make([]objectPropertyState, 0)
+	for rows.Next() {
+		var objectId string
+	    var objectType string
+	    var timestamp time.Time
+	    var property string
+	    var value string
+
+		err := rows.Scan(&objectId, &objectType, &timestamp, &property, &value)
+
+		if err != nil {
+	        fmt.Println(err)
+	        return
+	    }
+
+	    ops := objectPropertyState{
+            ObjectId: objectId,
+            ObjectType: objectType,
+            Timestamp: timestamp,
+            Property: property,
+            Value: value,
+        }
+        objectPropertyStateSlice = append(objectPropertyStateSlice, ops)
+	    
+	    fmt.Println(ops)
+	}
+	jsonOutput, err := json.MarshalIndent(objectPropertyStateSlice, "", "\t")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    fmt.Fprintln(w, string(jsonOutput))
+}
--- main.go
+++ main.go
@@ -4,12 +4,7 @@ import (
     "log"
     "net/http"
     "path/filepath"
-    "fmt"
     "github.com/gorilla/mux"
-    "strings"
-    "strconv"
-    "encoding/json"
-    "sort"
 )
 
 var uploadFolderName = "uploaded"
@@ -23,99 +18,4 @@ func main() {
     router.HandleFunc("/type/{type}/id/{id}", objectTypeId)
     router.HandleFunc("/type/{type}/id/{id}/time/{time}", objectTypeIdTime)
     log.Fatal(http.ListenAndServe(":9090", router))
-}
-
-func objectType(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	fmt.Fprintln(w, "Object Type is ", vars["type"])
-
-	records := parseCsv(w, r, "upload.csv")
-
-	for i := 0; i < len(records); i++ {
-		if strings.Compare(records[i][1], vars["type"]) == 0 {
-			for j := 0; j < len(records[i]); j++ {
-	            fmt.Fprintf(w, records[i][j] + " ")
-	        }
-	        fmt.Fprintln(w)
-		}
-	}
-}
-
-func objectTypeId(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	fmt.Fprintf(w, "Object Type is %v and Object ID is %v", vars["type"], vars["id"])
-	fmt.Fprintln(w)
-
-	records := parseCsv(w, r, "upload.csv")
-
-	for i := 0; i < len(records); i++ {
-		if strings.Compare(records[i][1], vars["type"]) == 0 && strings.Compare(records[i][0], vars["id"]) == 0 {
-			for j := 0; j < len(records[i]); j++ {
-	            fmt.Fprintf(w, records[i][j] + " ")
-	        }
-	        fmt.Fprintln(w)
-		}
-	}
-}
-
-func objectTypeIdTime(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	fmt.Fprintf(w, "Object Type is %v and Object ID is %v and the Time is %v", vars["type"], vars["id"], vars["time"])
-	fmt.Fprintln(w)
-
-	records := parseCsv(w, r, "upload.csv")
-
-	time, err := strconv.Atoi(vars["time"])
-	if err != nil {
-        fmt.Println(err)
-    }
-
-	properties := ""
-	var mapTimeToChanges map[int](map[string]string)
-
-	mapTimeToChanges = make(map[int](map[string]string))
-
-	for i := 0; i < len(records); i++ {
-		recordTimestamp, err := strconv.Atoi(records[i][2])
-		if err != nil {
-	        fmt.Println(err)
-	    }
-		if strings.Compare(records[i][1], vars["type"]) == 0 && strings.Compare(records[i][0], vars["id"]) == 0 && time >= recordTimestamp {
-			properties = properties + records[i][3] + ", "
-			for j := 0; j < len(records[i]); j++ {
-	            fmt.Fprintf(w, records[i][j] + " ")
-	        }
-	        fmt.Fprintln(w)
-			var dat map[string]string
-            if err := json.Unmarshal([]byte(records[i][3]), &dat); err != nil {
-		        fmt.Println(err)
-		    }
-		    mapTimeToChanges[recordTimestamp] = dat
-		    fmt.Println(dat)
-		    for key, value := range dat {
-			    fmt.Printf("key[%s] value[%s]\n", key, value)
-			}
-	        fmt.Fprintln(w)
-		}
-	}
-
-	var keys []int
-    for k := range mapTimeToChanges {
-        keys = append(keys, k)
-    }
-    sort.Ints(keys)
-
-    var objectStateMap map[string]string
-    objectStateMap = make(map[string]string)
-
-    for i := 0; i < len(keys); i++ {
-		var m map[string]string
-		m = make(map[string]string)
-		m = mapTimeToChanges[keys[i]]
-		for key, value := range m {
-		    fmt.Printf("key[%s] value[%s]\n", key, value)
-		    objectStateMap[key] = value
-		}
-    }
-    fmt.Fprintln(w, objectStateMap)
 }
